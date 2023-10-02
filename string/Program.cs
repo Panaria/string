abe8654c6bed780ca35d404e003c854f8d93f6b3
Panaria <baranyldzby@gmail.com>
2023-10-02 21:39:14 +0300

    Add project files.

---
+++ string/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Adınızı Giriniz");
+
+            string ad;
+            ad = Console.ReadLine();
+
+            Console.WriteLine("Hoş geldin " + ad);
+
+            Console.ReadLine(); 
+        }
+    }
+}
